5537889c3956a7fcfdd8b71cb024b82e1d1d10e6
Fiyinfoluwa Omotuyi <fiyinomotuyi@gmail.com>
2022-08-29 00:41:07 +0000

    completing project4 task

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="deanomotuyi/microproj"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy microproj --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward microproj 8000:80
